181f46d96df6db1587ee3d084c0437baa1c1fcd1
chenken <chenken@huohua.cn>
2022-06-07 10:30:07 +0800

    更新注释

--- shader/filter/gaussianBlur.fs.glsl
+++ shader/filter/gaussianBlur.fs.glsl
@@ -3,7 +3,7 @@ const float RADIUS = 15.;
 const float PI = 3.141592654;
 const float MY_E = 2.718281828;
 
-// the real gaussian blur ,whitch use the guassian fomula
+// 真正的高斯模糊---用高斯等式计算的高斯模糊
 float getWeight(float a, float b) {
     float deviation = 1.0;
     float n = -(a * a + b * b) / (2. * deviation * deviation);
@@ -17,14 +17,14 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     float sumWeight = 0.;
     vec2 textureSize = vec2(textureSize(iChannel0, 0));
     vec2 onePixelSize = vec2(1, 1) / textureSize;
-    // convolution
+    // 卷积
     for(float r = -RADIUS; r <= RADIUS; r++) {
         for(float c = -RADIUS; c <= RADIUS; c++) {
             vec2 targetUv = uv + vec2(r * onePixelSize.x, c * onePixelSize.y);
             if(targetUv.x < 0. || targetUv.x > 1. || targetUv.y < 0. || targetUv.y > 1.) {
                 continue;
             }
-            // simplify gaussian filter
+            // 简略的高斯filter
             // float weight = (RADIUS - abs(r)) * (RADIUS - abs(c));
             float weight = getWeight(r, c);
             color += texture(iChannel0, targetUv) * weight;
